f547b65dd32e454ddf0d7afa1ad74ca9b6abd3b9
Ben Sanchez <ben@example.com>
2025-10-01 03:30:11 -0700

    deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [vue()],
-  base: command === 'build' ? '/scso/' : '/'
-}))
+  base: '/scso/'
+})
