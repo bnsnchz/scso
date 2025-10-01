70ec868743ad7806d7ccc727baf05013ada11a82
Ben Sanchez <ben@example.com>
2025-10-01 03:32:57 -0700

    deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/scso/'
+  base: process.env.NODE_ENV === 'production' ? '/scso/' : '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
